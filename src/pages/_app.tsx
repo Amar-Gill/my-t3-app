7c1320d89cd287bc19572ef748bdceab708372c5
Amar Gill <amardeep@clearbanc.com>
2023-06-18 22:08:12 -0400

    create app nav

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,41 @@
 import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider, useSession } from "next-auth/react";
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import Link from "next/link";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  console.log(session);
   return (
     <SessionProvider session={session}>
+      <AppNav />
       <Component {...pageProps} />
     </SessionProvider>
   );
 };
 
+const AppNav: React.FC = () => {
+  const { data: session } = useSession();
+  if (!session?.user) {
+    return null;
+  }
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link className="mx-2" href="/">
+            Home
+          </Link>
+          <Link className="mx-2" href={"/users/" + session?.user.id.toString()}>
+            User
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 export default api.withTRPC(MyApp);
