23a9dd07913b51309b6db0e2fcc43f3bb6a354d7
Amar Gill <amardeep@clearbanc.com>
2023-06-27 22:50:13 -0400

    move post form and list into post page

--- src/pages/users/[id]/index.tsx
+++ src/pages/users/[id]/index.tsx
@@ -3,49 +3,13 @@ import { api } from "~/utils/api";
 import { type NextPage } from "next";
 import Image from "next/image";
 import { useSession } from "next-auth/react";
-import { FormEvent } from "react";
 
 const UserPage: NextPage = () => {
   const { query } = useRouter();
 
-  const utils = api.useContext();
-
   const userQuery = api.users.getById.useQuery(query.id as string);
 
-  const createPostMutation = api.posts.createPost.useMutation({
-    async onMutate(newPost) {
-      await utils.posts.listPosts.cancel();
-
-      const params = sessionData?.user.id.toString();
-
-      const prevData = utils.posts.listPosts.getData(params);
-
-      utils.posts.listPosts.setData(params, (old) => [...old, newPost]);
-
-      return { prevData };
-    },
-    onError(err, newPost, ctx) {
-      void utils.posts.listPosts.setData(
-        sessionData?.user.id.toString(),
-        ctx?.prevData
-      );
-    },
-    onSettled() {
-      void utils.posts.listPosts.invalidate();
-    },
-  });
-
   const { data: sessionData } = useSession();
-
-  function handleFormSubmit(e: FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    createPostMutation.mutate({
-      title: e.target.title.value,
-      content: e.target.content.value,
-      userId: userQuery.data?.id,
-      published: false,
-    });
-  }
 
   if (!sessionData) {
     return <div>Not logged in.</div>;
@@ -62,39 +26,6 @@ const UserPage: NextPage = () => {
           alt="profile image"
         />
       </div>
-      <form
-        onSubmit={handleFormSubmit}
-        className="flex flex-col border border-black bg-sky-600 p-2"
-      >
-        <label>Title</label>
-        <input name="title" type="text" className="m-1" />
-        <label>Content</label>
-        <input name="content" type="text" className="m-1" />
-        <button type="submit">Post</button>
-      </form>
-      {sessionData && <PostsLists />}
-    </>
-  );
-};
-
-const PostsLists: React.FC = () => {
-  const { data: sessionData } = useSession();
-  const listPostsQuery = api.posts.listPosts.useQuery(sessionData?.user.id);
-
-  if (!listPostsQuery.data) {
-    return <div>WOOPS</div>;
-  }
-
-  return (
-    <>
-      {listPostsQuery.data.map((p) => (
-        <div key={p.id}>
-          <p>id: {p.id}</p>
-          <p>title: {p.title}</p>
-          <p>content: {p.content}</p>
-          <p>published: {p.published ? "true" : "false"}</p>
-        </div>
-      ))}
     </>
   );
 };
--- src/pages/users/[id]/posts/index.tsx
+++ src/pages/users/[id]/posts/index.tsx
@@ -1,7 +1,81 @@
 import { NextPage } from "next";
+import { useSession } from "next-auth/react";
+import { FormEvent } from "react";
+import { api } from "~/utils/api";
 
 const PostPage: NextPage = () => {
-  return <div>Hello</div>;
+  const { data: sessionData } = useSession();
+
+  const utils = api.useContext();
+
+  const createPostMutation = api.posts.createPost.useMutation({
+    async onMutate(newPost) {
+      await utils.posts.listPosts.cancel();
+
+      const params = sessionData?.user.id.toString();
+
+      const prevData = utils.posts.listPosts.getData(params);
+
+      utils.posts.listPosts.setData(params, (old) => [...old, newPost]);
+
+      return { prevData };
+    },
+    onError(err, newPost, ctx) {
+      void utils.posts.listPosts.setData(
+        sessionData?.user.id.toString(),
+        ctx?.prevData
+      );
+    },
+    onSettled() {
+      void utils.posts.listPosts.invalidate();
+    },
+  });
+  function handleFormSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    createPostMutation.mutate({
+      title: e.target.title.value,
+      content: e.target.content.value,
+      userId: userQuery.data?.id,
+      published: false,
+    });
+  }
+  return (
+    <>
+      <form
+        onSubmit={handleFormSubmit}
+        className="flex flex-col border border-black bg-sky-600 p-2"
+      >
+        <label>Title</label>
+        <input name="title" type="text" className="m-1" />
+        <label>Content</label>
+        <input name="content" type="text" className="m-1" />
+        <button type="submit">Post</button>
+      </form>
+
+      {sessionData && <PostsLists />}
+    </>
+  );
 };
 
+const PostsLists: React.FC = () => {
+  const { data: sessionData } = useSession();
+  const listPostsQuery = api.posts.listPosts.useQuery(sessionData?.user.id);
+
+  if (!listPostsQuery.data) {
+    return <div>WOOPS</div>;
+  }
+
+  return (
+    <>
+      {listPostsQuery.data.map((p) => (
+        <div key={p.id}>
+          <p>id: {p.id}</p>
+          <p>title: {p.title}</p>
+          <p>content: {p.content}</p>
+          <p>published: {p.published ? "true" : "false"}</p>
+        </div>
+      ))}
+    </>
+  );
+};
 export default PostPage;
